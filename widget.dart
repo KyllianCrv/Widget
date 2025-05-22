d918954b21ce9ec5797f5e416397841e00a882ca
KyllianCrv <160327828+KyllianCrv@users.noreply.github.com>
2025-05-22 15:08:02 +0200

    Update widget.dart

--- widget.dart
+++ widget.dart
@@ -1,9 +1,25 @@
-import 'packagefluttermaterial.dart';
- 
-class HelloWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text(HelloWoooooooooooord, style TextStyle(fontSize 24));
-  }
+// Fichier modifiable à chaud
+Map<String, dynamic> get remoteWidget {
+  return {
+    'type': 'Container',
+    'decoration': {
+      'color': '#FF3366',
+      'borderRadius': 20.0,
+    },
+    'child': {
+      'type': 'Column',
+      'children': [
+        {
+          'type': 'Text',
+          'data': 'Mis à jour: ${DateTime.now()}',
+          'style': {'fontSize': 24}
+        },
+        {
+          'type': 'CustomButton',
+          'label': 'Actualiser',
+          'action': 'refresh'
+        }
+      ]
+    }
+  };
 }
-
