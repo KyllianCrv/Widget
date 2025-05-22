a34b6ae19665bc2315989e3dc3a6fd5cf443e538
KyllianCrv <160327828+KyllianCrv@users.noreply.github.com>
2025-05-22 14:18:36 +0200

    Update widget.dart

--- widget.dart
+++ widget.dart
@@ -3,7 +3,7 @@ import 'packagefluttermaterial.dart';
 class HelloWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text(HelloW, style TextStyle(fontSize 24));
+    return Text(HelloWord, style TextStyle(fontSize 24));
   }
 }
 
