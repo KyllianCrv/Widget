6c146683524b0ef1fea831d3e2ea8caa7ca53368
KyllianCrv <160327828+KyllianCrv@users.noreply.github.com>
2025-05-22 14:08:06 +0200

    Add files via upload

---
+++ widget.dart
@@ -0,0 +1,9 @@
+import 'packagefluttermaterial.dart';
+ 
+class HelloWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(HelloW, style TextStyle(fontSize 24));
+  }
+}
+
